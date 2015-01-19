e8737b0232b6b51838ccd9d926030e5c2ad5155d
Stanimir Mitev <hunterinthestorm@gmail.com>
2015-01-19 16:28:56 +0200

    Update 01.rb

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,23 +1,18 @@
 def fibonacci(number)
-  if number < 2
-    number
-  else
-    fibonacci(number - 1) + fibonacci(number - 2)
-  end
+  return number if number < 2
+  fibonacci(number - 1) + fibonacci(number - 2)
 end
 
 def lucas(number)
-  case number
-    when 1 then 2
-    when 2 then 1
-    else lucas(number - 1) + lucas(number - 2)
-  end
+  return 1 if number == 2
+  return 2 if number == 1
+  lucas(number - 1) + lucas(number - 2)
 end
 
 def series(name, number)
   case name
-    when "lucas"     then series(number)
-    when "fibonacci" then fibonacci(number)
+    when "lucas"     then lucas     number
+    when "fibonacci" then fibonacci number
     when "summed"    then fibonacci(number) + lucas(number)
   end
 end
